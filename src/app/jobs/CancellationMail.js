985b2d16d2dd9d00b324339d6c49d748d57457d9
DevTeles <rteles78@gmail.com>
2019-10-28 10:32:27 -0300

    GoBarber - backend

---
+++ src/app/jobs/CancellationMail.js
@@ -0,0 +1,35 @@
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Mail from '../../lib/Mail';
+
+class CancellationMail {
+  get key() {
+    return 'CancellationMail';
+  }
+
+  async handle({ data }) {
+    const { appointment } = data;
+
+    console.log('A fila executou');
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(
+          parseISO(appointment.date),
+          "'dia' dd 'de' MMMM', às' H:mm'h'",
+          {
+            locale: pt,
+          }
+        ),
+      },
+      // text: 'Você tem um novo cancelamento',
+    });
+  }
+}
+
+export default new CancellationMail();
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://92780748d87f40c99c934d9f06d03069@sentry.io/1794981',
+};
